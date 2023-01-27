704495e1d39712b18e69bf618772adb04b5f2e6d
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-01-27 15:38:29 +0100

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-       
+
   validates :name,
             length: { minimum: 3, message: 'fields can not be less than 3 letters' }, presence: true
- validates :email,
+  validates :email,
             uniqueness: true, presence: true
- validates :encrypted_password,
+  validates :encrypted_password,
             length: { minimum: 6, message: 'can not be less than 6 characters' }, presence: true
- validates :username,
+  validates :username,
             length: { minimum: 6, message: 'can not be less than 6 characters' }, presence: true
 end
