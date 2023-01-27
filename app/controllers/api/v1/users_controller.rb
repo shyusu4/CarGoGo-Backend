84ef001aa9b0afa0123ba597e2eec0213d9592b6
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-01-27 10:28:42 +0100

    Added controller to get all users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+
+    def index
+        @users = User.all
+    
+        render json: { status: 200, data: @users }
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :api do
+    namespace :v1 do
+        # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+      resources :users, only: [:create, :destroy]
+    end
+  end
 end
