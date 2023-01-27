6d2b3a18ad178a1cb3209c0fe23eb8d3aa1984e4
roseokpe1~ <roseokpe@gmail.com>
2023-01-27 08:21:57 -0800

    setup car model

---
+++ app/models/car.rb
@@ -0,0 +1,11 @@
+class Car < ApplicationRecord
+    belongs_to: favorite
+
+    validates: model, presence: true
+    validates: color, presence: true
+    validates: description, presence: true
+    validates: brand, presence: true
+    validates: image, presence: true
+    validates: price, presence: true, numericality: { greater_than: 0 }
+
+end
