044e9fca49286987f28b3e061ce45ab54e8cdee1
riyabulia12 <riyabulia12@gmail.com>
2023-01-30 21:34:54 +0530

    Add model and migrate favorite entity

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_27_073326) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_131155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "model"
+    t.string "color"
+    t.string "description"
+    t.date "production_date"
+    t.string "brand"
+    t.string "image"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_favorites_on_car_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -28,4 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_27_073326) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "cars"
+  add_foreign_key "favorites", "users"
 end
